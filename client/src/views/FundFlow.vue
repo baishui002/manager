17daf0e0dbc6eb99cf18b745439cc0a8003698a8
baishui <13809898824@139.com>
2019-08-15 21:50:49 +0800

    fundList table

--- client/src/views/FundFlow.vue
+++ client/src/views/FundFlow.vue
@@ -8,83 +8,160 @@
         <el-button type="primary" class="filter">筛选</el-button>
       </el-col>
       <el-col :span="2" class="add-box">
-        <el-button type="primary" class="add">添加</el-button>
+        <el-button type="primary" class="add" @click="handleAdd">添加</el-button>
       </el-col>
     </el-row>
     <el-row class="content">
-      <el-table :data="tableData" border style="width: 100%">
-        <el-table-column fixed prop="date" label="日期" width="150"></el-table-column>
-        <el-table-column prop="name" label="姓名" width="120"></el-table-column>
-        <el-table-column prop="province" label="省份" width="120"></el-table-column>
-        <el-table-column prop="city" label="市区" width="120"></el-table-column>
-        <el-table-column prop="address" label="地址" width="300"></el-table-column>
-        <el-table-column prop="zip" label="邮编" width="120"></el-table-column>
-        <el-table-column fixed="right" label="操作" width="100">
+      <el-table :data="tableData" border style="width: 100%" max-height="400px">
+        <el-table-column label="序号" width="50" type="index" align="center"></el-table-column>
+        <el-table-column label="创建时间" width="150" align="center">
           <template slot-scope="scope">
-            <el-button @click="handleClick(scope.row)" type="text" size="small">查看</el-button>
-            <el-button type="text" size="small">编辑</el-button>
+            <i class="el-icon-time"></i>
+            <span style="margin-left: 10px">{{ scope.row.date }}</span>
+          </template>
+        </el-table-column>
+        <el-table-column label="收入类型" width="120" align="center">
+          <template slot-scope="scope">
+            <span style="margin-left: 10px">{{ scope.row.type }}</span>
+            <!-- <el-popover trigger="hover" placement="top">
+              <p>姓名: {{ scope.row.name }}</p>
+              <p>住址: {{ scope.row.address }}</p>
+              <div slot="reference" class="name-wrapper">
+                <el-tag size="medium">{{ scope.row.name }}</el-tag>
+              </div>
+            </el-popover>-->
+          </template>
+        </el-table-column>
+        <el-table-column label="收入描述" width="180" align="center" style="{color: red}">
+          <template slot-scope="scope">
+            <span style="margin-left: 10px">{{ scope.row.describe }}</span>
+          </template>
+        </el-table-column>
+        <el-table-column label="收入" width="120" align="center">
+          <template slot-scope="scope">
+            <span style="color:#00d053">{{ scope.row.incode }}</span>
+          </template>
+        </el-table-column>
+        <el-table-column label="支出" width="120" align="center">
+          <template slot-scope="scope">
+            <span style="color: red">{{ scope.row.expend }}</span>
+          </template>
+        </el-table-column>
+        <el-table-column label="账户现金" width="120" align="center">
+          <template slot-scope="scope">
+            <span style="color: skyblue">{{ scope.row.cash }}</span>
+          </template>
+        </el-table-column>
+        <el-table-column label="备注" width="180" align="center">
+          <template slot-scope="scope">
+            <span>{{ scope.row.remark }}</span>
+          </template>
+        </el-table-column>
+        <el-table-column label="操作" width="180" align="center" fixed="right">
+          <template slot-scope="scope">
+            <el-button size="mini" type="warning" @click="handleEdit(scope.$index, scope.row)">编辑</el-button>
+            <el-button size="mini" type="danger" @click="handleDelete(scope.$index, scope.row)">删除</el-button>
           </template>
         </el-table-column>
       </el-table>
     </el-row>
+    <!-- <Dialog :dialogShow="dialogShow" /> -->
+    <el-dialog title="收货地址" :visible.sync="dialogShow">
+      <el-form :model="form">
+        <el-form-item label="活动名称" :label-width="formLabelWidth">
+          <el-input v-model="form.name" autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="活动区域" :label-width="formLabelWidth">
+          <el-select v-model="form.region" placeholder="请选择活动区域">
+            <el-option label="区域一" value="shanghai"></el-option>
+            <el-option label="区域二" value="beijing"></el-option>
+          </el-select>
+        </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="dialogFormVisible = false">取 消</el-button>
+        <el-button type="primary" @click="dialogFormVisible = false">确 定</el-button>
+      </div>
+    </el-dialog>
   </div>
 </template>
 
 <script>
+import { Dialog } from "../components/Dialog";
 export default {
   name: "Money",
   data() {
     return {
+      dialogShow: false,
       date1: "",
       date2: "",
       tableData: [
         {
           date: "2016-05-02",
-          name: "王小虎",
-          province: "上海",
-          city: "普陀区",
-          address: "上海市普陀区金沙江路 1518 弄",
-          zip: 200333
+          type: "提现",
+          describe: "上海市",
+          incode: "300",
+          expend: "100",
+          cash: "200",
+          remark: "上海市普8 弄"
         },
         {
           date: "2016-05-04",
-          name: "王小虎",
-          province: "上海",
-          city: "普陀区",
-          address: "上海市普陀区金沙江路 1517 弄",
-          zip: 200333
+          type: "提现",
+          describe: "上海市",
+          incode: "300",
+          expend: "100",
+          cash: "200",
+          remark: "上海市普8 弄"
         },
         {
-          date: "2016-05-04",
-          name: "王小虎",
-          province: "上海",
-          city: "普陀区",
-          address: "上海市普陀区金沙江路 1517 弄",
-          zip: 200333
+          date: "2016-05-01",
+          type: "提现",
+          describe: "上海市",
+          incode: "300",
+          expend: "100",
+          cash: "200",
+          remark: "上海市普8 弄"
         },
         {
-          date: "2016-05-04",
-          name: "王小虎",
-          province: "上海",
-          city: "普陀区",
-          address: "上海市普陀区金沙江路 1517 弄",
-          zip: 200333
-        },
-        {
-          date: "2016-05-04",
-          name: "王小虎",
-          province: "上海",
-          city: "普陀区",
-          address: "上海市普陀区金沙江路 1517 弄",
-          zip: 200333
+          date: "2016-05-03",
+          type: "提现",
+          describe: "上海市",
+          incode: "300",
+          expend: "100",
+          cash: "200",
+          remark: "上海市普8 弄"
         }
-      ]
+      ],
+      form: {
+        name: "",
+        region: "",
+        date1: "",
+        date2: "",
+        delivery: false,
+        type: [],
+        resource: "",
+        desc: ""
+      },
+      formLabelWidth: "120px"
     };
   },
   methods: {
+    handleAdd() {
+      this.dialogShow = true;
+    },
     handleClick(row) {
       console.log(row);
+    },
+    handleEdit(index, row) {
+      console.log(index, row);
+    },
+    handleDelete(index, row) {
+      console.log(index, row);
     }
+  },
+  components: {
+    Dialog
   }
 };
 </script>
@@ -92,9 +169,10 @@ export default {
 <style lang="scss" scoped>
 @import "@/assets/css/layout.scss";
 #money {
+  // @include WH(100%, 100%);
   .top {
-    height: 80px;
-    line-height: 80px;
+    height: 60px;
+    line-height: 60px;
     .time-box {
       padding-left: 10px;
       min-width: 720px;
@@ -112,7 +190,10 @@ export default {
   }
 
   .content {
-    padding: 10px;
+    padding: 0 10px;
+    .btn-look {
+      margin-right: 10px;
+    }
   }
 }
 </style>
---
+++ client/src/views/fundFlow_bk.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+            <el-table :data="tableData" border style="width: 100%" max-height="400">
+        <el-table-column type="index" label="序号" width="150"></el-table-column>
+        <el-table-column prop="date" label="日期" width="150"></el-table-column>
+        <el-table-column prop="name" label="姓名" width="120"></el-table-column>
+        <el-table-column prop="province" label="省份" width="120"></el-table-column>
+        <el-table-column prop="city" label="市区" width="120"></el-table-column>
+        <el-table-column prop="address" label="地址" width="300"></el-table-column>
+        <el-table-column prop="zip" label="邮编" width="120"></el-table-column>
+        <el-table-column fixed="right" label="操作" width="200" align="center">
+          <template slot-scope="scope">
+            <el-button
+              @click="handleClick(scope.row)"
+              type="warning"
+              size="mini"
+              class="btn-look"
+            >查看</el-button>
+            <el-button type="danger" size="mini">编辑</el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+  </div>
+</template>
+
+<script>
+export default {
+  name:"",
+  data() {
+    return {
+
+    };
+  },
+  methods: {
+
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
